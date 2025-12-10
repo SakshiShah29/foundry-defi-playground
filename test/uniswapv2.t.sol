263019c1eb4c5c6c2cfe216fe13c9d2cdc639201
SakshiShah29 <sakshi.shah299@gmail.com>
2025-12-10 11:49:27 +0530

    feat:twap tests uniswap-v2

--- test/uniswapv2.t.sol
+++ test/uniswapv2.t.sol
@@ -24,6 +24,7 @@ import {IWETH} from "../src/interfaces/IWETH.sol";
 import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
 import {TestToken} from "../src/TestToken.sol";
 import {UniswapV2FlashSwap} from "./../src/uniswap-v2/UniswapV2FlashSwap.sol";
+import {UniswapV2Twap} from "./../src/uniswap-v2/UniswapV2Twap.sol";
 
 contract UniswapV2SwapAmountsTest is Test {
     IERC20 private constant weth = IERC20(WETH);
@@ -37,10 +38,13 @@ contract UniswapV2SwapAmountsTest is Test {
 
     IUniswapV2Pair private constant pair =
         IUniswapV2Pair(UNISWAP_V2_PAIR_DAI_WETH);
-          UniswapV2FlashSwap private flashSwap;
-
+    UniswapV2FlashSwap private flashSwap;
+    UniswapV2Twap private twap;
+    uint256 private constant MIN_WAIT = 300;
     function setUp() public {
         flashSwap = new UniswapV2FlashSwap(UNISWAP_V2_PAIR_DAI_WETH);
+        twap = new UniswapV2Twap(address(pair));
+
         deal(user, 1000 * 1e18);
         vm.startPrank(user);
         IWETH(WETH).deposit{value: 100 * 1e18}();
@@ -267,5 +271,70 @@ contract UniswapV2SwapAmountsTest is Test {
 
         console.log("DAI fee", dai1 - dai0);
         assertGe(dai1, dai0, "DAI balance of pair");
+    }
+
+    function test_twap_same_price() public {
+        skip(MIN_WAIT + 1);
+        twap.update();
+
+        uint256 twap0 = twap.consult(WETH, 1e18);
+
+        skip(MIN_WAIT + 1);
+        twap.update();
+
+        uint256 twap1 = twap.consult(WETH, 1e18);
+
+        assertApproxEqAbs(twap0, twap1, 1, "ETH TWAP");
+    }
+
+    function test_twap_close_to_last_spot() public {
+        // Update TWAP
+        skip(MIN_WAIT + 1);
+        twap.update();
+
+        // Get TWAP
+        uint256 twap0 = twap.consult(WETH, 1e18);
+
+        // Swap
+        swap();
+        uint256 spot = getSpot();
+        console.log("ETH spot price", spot);
+
+        // Update TWAP
+        skip(MIN_WAIT + 1);
+        twap.update();
+
+        // Get TWAP
+        uint256 twap1 = twap.consult(WETH, 1e18);
+
+        console.log("twap0", twap0);
+        console.log("twap1", twap1);
+
+        // Check TWAP is close to last spot
+        assertLt(twap1, twap0, "twap1 >= twap0");
+        assertGe(twap1, spot, "twap1 < spot");
+    }
+    function getSpot() internal view returns (uint256) {
+        (uint112 reserve0, uint112 reserve1, ) = pair.getReserves();
+        // DAI / WETH
+        return (uint256(reserve0) * 1e18) / uint256(reserve1);
+    }
+
+    function swap() internal {
+        deal(WETH, address(this), 100 * 1e18);
+        weth.approve(address(router), type(uint256).max);
+
+        address[] memory path = new address[](2);
+        path[0] = WETH;
+        path[1] = DAI;
+
+        // Input token amount and all subsequent output token amounts
+        uint256[] memory amounts = router.swapExactTokensForTokens({
+            amountIn: 100 * 1e18,
+            amountOutMin: 1,
+            path: path,
+            to: address(this),
+            deadline: block.timestamp
+        });
     }
 }
